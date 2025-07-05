c1dad35da5d3281293f4a6e1190d8253ee14e9d6
dipanshudeep <dipanshu200303@gmail.com>
2025-07-05 23:59:43 +0530

    login rout added

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -48,4 +48,44 @@ const register = async (req, res) => {
              
     }
 
-export {register}
+const login = async (req, res) => {
+    try {
+         const {email, password} = req.body
+         if(!email || !password){
+            return res.status(400).json({success:false, message: "all fields are required."});
+         }
+
+         const user = await admin.findOne({email})
+         if (!user) {
+            return res.status(400).json({success: false, message: "Invalid email or password."})
+         }
+
+         const isPasswordValid = await bcrypt.compare(password, user.password)
+         if (!isPasswordValid){
+            return res.status(400).json({success: false, message: "Invalid email or password."})
+         }
+         const token = jwt.sign({id: user._id}, process.env.jwt_SECRET,{expiresIn: '7d'});
+          res.cookie("token", token, {
+            httpOnly: false,
+            secure: false,
+            sameSite: 'lax',
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+          })
+
+          const userResponse = {
+            id : user._id,
+            username : user.username,
+            email : user.email,
+          }
+          res.status(200).json({success : true, message : 'login successful', user: userResponse, token});
+
+
+    } catch (error) {
+        console.error("Error in admin login:", error);
+        return res.status(500).json({success: false, message:"Internal server error."});
+        
+    }
+}
+
+
+export {register, login};
--- backend/routes/admin.router.js
+++ backend/routes/admin.router.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { register } from '../controllers/admin.controller.js'
+import { login, register } from '../controllers/admin.controller.js'
 
 
 const adminRouter = express.Router()
 
 adminRouter.post('/register',register)
+adminRouter.post('/login',login)
 
 export default adminRouter
